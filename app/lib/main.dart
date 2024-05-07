a1ef7b72b9a395f9d0d15b272ee7163d4874e0e3
CarlijnGJ <carlijn.blom@windesheim.nl>
2024-05-07 18:00:43 +0200

    First draft homepage

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -9,11 +9,134 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        colorScheme: const ColorScheme(
+          primary: Color(0xFF4E4E4E),
+          onPrimary: Colors.white,
+          secondary: Color.fromARGB(255, 0, 75, 16),
+          onSecondary: Colors.white,
+          tertiary: Colors.black,
+          onTertiary: Colors.white,
+          background: Color(0xFF4E4E4E),
+          onBackground: Colors.white,
+          surface: Colors.black,
+          onSurface: Colors.white,
+          error: Colors.red,
+          onError: Colors.white,
+          brightness: Brightness.dark
         ),
+      ),  
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              stops: [
+                0.05,
+                0.15,
+                0.5,
+                0.85,
+                0.95,
+              ],
+              colors: [
+                Colors.teal,
+                Colors.black,
+                Colors.black,
+                Colors.black,
+                Colors.teal
+              ],
+            ),
+          ),
+          child: const SingleChildScrollView(
+            child: Column(
+              children: [
+                TitleSection(
+                    name: 'Near Mint Mobile', 
+                    location: 'Steenwijk, Nederland'
+                  ),
+                TextSection(
+                  description:
+                  'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
+                  'Bernese Alps. Situated 1,578 meters above sea level, it '
+                  'is one of the larger Alpine Lakes. A gondola ride from '
+                  'Kandersteg, followed by a half-hour walk through pastures '
+                  'and pine forest, leads you to the lake, which warms to 20 '
+                  'degrees Celsius in the summer. Activities enjoyed here '
+                  'include rowing, and riding the summer toboggan run.',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  location,
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({
+    super.key,
+    required this.description,
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
       ),
     );
   }
