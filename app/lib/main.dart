763359168977df9a551807b88d2ac50da83c7a1e
agebrouwer <s1129646@student.windesheim.nl>
2024-05-07 14:16:54 +0200

    Kleine basis appbar
    basis home page

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,19 +1,40 @@
+import 'package:app/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+/// Flutter code sample for [AppBar].
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+void main() => runApp(const AppBarApp());
+
+class AppBarApp extends StatelessWidget {
+  const AppBarApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      home: TopBar(),
+    );
+  }
+}
+
+class TopBar extends StatelessWidget {
+  const TopBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Near Mint Gaming'),
+        actions: const <Widget>[
+          Padding(
+              padding: EdgeInsets.only(right: 20.0, top: 5),
+              child: Icon(Icons.more_horiz)),
+          Padding(
+              padding: EdgeInsets.only(right: 20.0, top: 20),
+              child: Text('Login')),
+        ],
+      ),
+      body: const Center(
+        child: HomePage(),
       ),
     );
   }
