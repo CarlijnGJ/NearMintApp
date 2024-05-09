890883a24c5ecd13b48c62f1ce612d5a45f57e0f
Jelle6837 <s1181756@student.windesheim.nl>
2024-05-09 15:05:09 +0200

    login page layout

--- app/lib/screens/login/login_screen.dart
+++ app/lib/screens/login/login_screen.dart
@@ -1,5 +1,6 @@
+import 'package:app/screens/login/components/button.dart';
 import 'package:flutter/material.dart';
-
+import 'package:app/screens/login/components/textfield.dart';
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -8,9 +9,81 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
+
+
 class _LoginPageState extends State<LoginPage> {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void loginUser(){  print("Login button pressed!");}
+  void forgotPassword(){  print("Forgot Password button pressed!");}
   @override
   Widget build(BuildContext context) {
-    return const Text('Login Page');
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body:  SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+
+              const Text(
+                'Credentials',
+                style: TextStyle(
+                  fontSize: 20,
+                ),),
+              const SizedBox(height: 20),
+
+              // username textfield
+              CustomTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+
+              // password textfield
+              CustomTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: forgotPassword,
+                      child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              CustomButton(
+                onTap: loginUser),
+              
+              const SizedBox(height: 30),
+              
+              Divider(),
+
+              Text('or use the QR code'),
+
+              Icon(Icons.qr_code_2_rounded, size: 180),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
