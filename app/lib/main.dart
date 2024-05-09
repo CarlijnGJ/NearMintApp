1bced17f6dee11c87407066cf209de5bd6d3712e
Jelle6837 <s1181756@student.windesheim.nl>
2024-05-09 15:13:18 +0200

    Revert "navbar"
    This reverts commit aa5837066b1f6efe3e15f2c77ff2d0f4d36e45c2.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,75 +1,51 @@
 import 'package:app/components/topbar.dart';
-import 'package:app/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:app/screens/home/home_screen.dart'; // Import the necessary widgets
 
 void main() => runApp(const AppBarApp());
-
-class CommonScaffold extends StatelessWidget {
-  final Widget body;
-
-  const CommonScaffold({Key? key, required this.body}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      
-      appBar: TopBar(),
-      body: body,
-    );
-  }
-}
 
 class AppBarApp extends StatelessWidget {
   const AppBarApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // themeAnimationStyle: AnimationStyle.noAnimation,
-      routes: {
-        '/': (context) => CommonScaffold(body: HomePage()),
-        '/login': (context) => CommonScaffold(body: LoginPage()),
-        //         '/': (context) => const HomePage(), // Home route
-        // '/login': (context) => const LoginPage(), // Login page route
-      },
-      
-      
+    return const MaterialApp(
+      home: MainPage(),
     );
   }
 }
 
-// class MainPage extends StatefulWidget {
-//   const MainPage({Key? key}) : super(key: key);
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
 
-//   @override
-//   _MainPageState createState() => _MainPageState();
-// }
+  @override
+  _MainPageState createState() => _MainPageState();
+}
 
-// class _MainPageState extends State<MainPage> {
-//   late Widget currentScreen;
+class _MainPageState extends State<MainPage> {
+  late Widget currentScreen;
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     currentScreen = const HomePage();
-//   }
+  @override
+  void initState() {
+    super.initState();
+    currentScreen = const HomePage();
+  }
 
-//   void setCurrentScreen(Widget screen) {
-//     setState(() {
-//       currentScreen = screen;
-//     });
-//   }
+  void setCurrentScreen(Widget screen) {
+    setState(() {
+      currentScreen = screen;
+    });
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: TopBar(
-//         onScreenChange: setCurrentScreen, // Pass the callback function
-//       ),
-//       body: Center(
-//         child: currentScreen,
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TopBar(
+        onScreenChange: setCurrentScreen, // Pass the callback function
+      ),
+      body: Center(
+        child: currentScreen,
+      ),
+    );
+  }
+}
