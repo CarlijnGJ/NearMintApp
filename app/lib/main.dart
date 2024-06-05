1fdd416f17fccf2ee2050d1066ebe40ee9032e9c
Jelle6837 <s1181756@student.windesheim.nl>
2024-06-05 11:59:21 +0200

    editmember screen

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app/screens/addmember/addmember_screen.dart';
 import 'package:app/screens/addtransaction/addtransaction.dart';
+import 'package:app/screens/editmember/editmember.dart';
 import 'package:app/screens/members/components/user.dart';
 import 'package:app/screens/members/member_list.dart';
 import 'package:app/screens/profile/profile_screen.dart';
@@ -8,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:app/components/themes/maintheme.dart'; //import the main theme for the app
 import 'package:app/screens/home/home_screen.dart'; // Import the necessary widgets
 import 'package:app/screens/login/login_screen.dart';
-import 'package:app/services/lifecycle_observer.dart'; // Import the lifecycle observer
 
 void main() async {
   //WidgetsFlutterBinding.ensureInitialized();
@@ -36,14 +36,22 @@ class AppBarApp extends StatelessWidget {
         '/profile': (context) => const ProfilePage(),
         '/addmember': (context) => const AddMemberPage(),
         '/setup': (context) => const SetupPage(),
-'/addtransaction': (context) {
-  final arguments = ModalRoute.of(context)!.settings.arguments;
-  if (arguments == null || !(arguments is User)) {
-    return AddTransactionPage(member: null);
-  }
-  final User member = arguments as User;
-  return AddTransactionPage(member: member);
-},
+        '/addtransaction': (context) {
+          final arguments = ModalRoute.of(context)!.settings.arguments;
+          if (arguments == null || !(arguments is User)) {
+            return const AddTransactionPage(member: null);
+          }
+          final User member = arguments as User;
+          return AddTransactionPage(member: member);
+        },
+        '/editmember': (context) {
+          final arguments = ModalRoute.of(context)!.settings.arguments;
+          if (arguments == null || !(arguments is String)) {
+            return EditMemberPage(sessionKey: '');
+          }
+          final String sessionKey = arguments as String;
+          return EditMemberPage(sessionKey: sessionKey);
+        },
     },
     );
   }
