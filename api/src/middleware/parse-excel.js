b37129e64d4ab77cfa3779f91d7ff75277b35067
agebrouwer <s1129646@student.windesheim.nl>
2024-06-17 22:02:16 +0200

    added functionality to import excel files

--- api/src/middleware/parse-excel.js
+++ api/src/middleware/parse-excel.js
@@ -1,11 +1,12 @@
 const xlsx = require('xlsx');
 
-function parseExcel(filePath) {
+function parsingExcelFile(filePath) {
     const workbook = xlsx.readFile(filePath);
     const sheetName = workbook.SheetNames[0];
     const sheet = workbook.Sheets[sheetName];
     const data = xlsx.utils.sheet_to_json(sheet);
-  
+    let filteredDataArray = [];
+
     const filteredData = data.map(row => {
       let filteredRow = {};
       Object.keys(row).forEach(key => {
@@ -14,9 +15,9 @@ function parseExcel(filePath) {
         }
       });
       return filteredRow;
-    }).filter(row => Object.keys(row).length > 0);
-  
+    }).filter(row => Object.keys(row).length > 0).forEach(row => {filteredDataArray.push(row)});
     console.log(filteredData);
-  };
+    return filteredDataArray;
+};
 
-module.exports = parseExcel;
+module.exports = parsingExcelFile;
